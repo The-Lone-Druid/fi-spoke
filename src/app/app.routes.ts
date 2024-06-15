1c8d5cba3e5a36a5a6808073ebebd5142094ac11
Zahid Shaikh <zahid.786shaikh@gmail.com>
2024-06-15 19:07:57 +0530

    feat: layouts added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // temporary redirect
+  {
+    path: '',
+    pathMatch: 'prefix',
+    redirectTo: 'dashboard',
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./pages/auth-layout/auth-layout.module').then(
+        (m) => m.AuthLayoutModule
+      ),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./pages/dashboard-layout/dashboard-layout.module').then(
+        (m) => m.DashboardLayoutModule
+      ),
+  },
+];
---
+++ src/app/pages/dashboard-layout/liquidity-dashboard/liquidity-dashboard.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LiquidityDashboardRoutingModule } from './liquidity-dashboard-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LiquidityDashboardRoutingModule
+  ]
+})
+export class LiquidityDashboardModule { }
