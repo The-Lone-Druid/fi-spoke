047e7fbd556d1f2d58d943752b4afa4318479838
Zahid Shaikh <zahid.786shaikh@gmail.com>
2024-06-15 22:32:48 +0530

    feat: implemented dashboard and auth layout and also customized theme

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { SidebarService } from '../../shared/sidebar-service/sidebar.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, AvatarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { MenuItemComponent } from '../menu-item/menu-item.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, RouterModule, ButtonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
+  imports: [CommonModule, RouterModule, ButtonModule, MenuItemComponent],
 })
 export class SidebarComponent implements OnInit {
   constructor() {}
